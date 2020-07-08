a702405802ee884cad68e73bfe90f9e940e37323
Danish Tamboli <danishtamboli123@gmail.com>
2020-07-08 23:41:59 +0530

    Adding remaining project files and Dataset zip
    Signed-off-by: Danish Tamboli <danishtamboli123@gmail.com>

---
+++ household_power_consumption.R
@@ -0,0 +1,68 @@
+# Check for if Zip has been Downloaded,else to download.
+if(!file.exists("Electricity_Power_Consumptipon.zip")){
+  download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip","Electricity_Power_Consumptipon.zip")
+}
+# Check for if Zip has been Unzipped,else to unzip.
+if(!file.exists("Electricity_Power_Consumptipon")){
+  unzip("Electricity_Power_Consumptipon.zip")
+}
+
+# Load the Enhanced data.frame library (Aka data.table)
+library(data.table)
+
+# Read and store the Household Power Consumption Data.
+household_power_consumption <- fread("household_power_consumption.txt")
+
+# Converting charachter representation of Data field in Table to Date of class Date
+household_power_consumption$Date <- as.Date(household_power_consumption$Date,'%d/%m/%Y')
+
+# Subsetting the required dates for analysis.
+required_hpc <- subset(household_power_consumption,household_power_consumption$Date >= "2007-02-01" & household_power_consumption$Date <= "2007-02-02")
+
+# Plotting Histogram of Global Active Power usage in Kilowatts between the period in PNG.
+png(width= 480,height = 480,filename = "plot1.png")
+with(required_hpc,hist(as.numeric(Global_active_power), xlab = "Global Active Power (kilowatts)",col = "red", main= "Global Active Power"))
+dev.off()
+
+# Load the lubridate library.
+library(lubridate)
+
+
+days <- with(required_hpc,paste(Date,Time))
+days <- strptime(days, "%Y-%m-%d %H:%M:%S")
+
+# Plotting Change in Global Active Power (Kilowatts) with respect to Days.
+png(width= 480,height = 480,filename = "plot2.png")
+plot(required_hpc$days,required_hpc$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (Kilowatts)")
+dev.off()
+
+# Plotting The three Engergy Sub metering readings across different Days.
+png(width= 480,height = 480,filename = "plot3.png")
+with(required_hpc,{
+  plot(days,Sub_metering_1,type = "n",ylab = "Energy Sub metering",xlab = "")
+  lines(days,Sub_metering_1,col = "black")
+  lines(days,Sub_metering_2,col = "red")
+  lines(days,Sub_metering_3, col = "blue")
+  legend("topright",legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col = c("black","red","blue"),lty = 1)
+})
+dev.off()
+
+# Plotting in quadrant with par and mfrow the required 4 plots.
+# 1. Days vs Global Active Power
+# 2. Days vs Voltage
+# 3. Days vs Sub Metering readings
+# 4. Days vs Gloval Reactive Power
+png(width= 480,height = 480,filename = "plot4.png")
+par(mfrow= c(2,2))
+with(required_hpc,
+     {
+       plot(days,Global_active_power,xlab = "",ylab = "Global Active Power",col = "black", type = "l")
+       plot(days,Voltage,xlab = "datetime", ylab = "Voltage",type = "l")
+       plot(days,Sub_metering_1,type = "n",ylab = "Energy Sub metering",xlab = "")
+       lines(days,Sub_metering_1,col = "black")
+       lines(days,Sub_metering_2,col = "red")
+       lines(days,Sub_metering_3, col = "blue")
+       legend("topright",legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col = c("black","red","blue"),lty = 1)
+       plot(days,Global_reactive_power,type = "l",xlab = "datetime")
+     })
+dev.off()
